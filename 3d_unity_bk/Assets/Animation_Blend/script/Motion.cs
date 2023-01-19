b8ca0c33bc0a64b826c2d8a84c803949fdf38b80
Administrator <sody15963@gmail.com>
2023-01-19 21:48:26 +0900

    애니메이션블렌드

---
+++ 3d_unity_bk/Assets/Animation_Blend/script/Motion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motion : MonoBehaviour
+{
+    private Animator animator;
+
+
+    void Start()
+    {
+        animator= GetComponent<Animator>(); 
+    }
+
+   
+    void Update()
+    {
+        // Input.GetAxis = -1~0~1 사이의 값을 반환. 
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("Vertical", vertical);
+        animator.SetFloat("Horizontal", horizontal);
+
+    }
+}
