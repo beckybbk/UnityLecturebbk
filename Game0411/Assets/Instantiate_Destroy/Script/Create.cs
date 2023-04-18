c09d49699eb591b40376635224e1f6f69d93efe0
beckybbk <120100729+beckybbk@users.noreply.github.com>
2023-04-18 19:47:34 +0900

    애니메이션클립완성0418

---
+++ Game0411/Assets/Instantiate_Destroy/Script/Create.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Create : MonoBehaviour
+{
+    public Button button;
+    public GameObject prefab;
+
+    private bool active = true;
+    private float fixedTime = 5f;
+    private float currentTime = 5f;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
