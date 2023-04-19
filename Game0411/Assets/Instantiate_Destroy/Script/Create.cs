ea9c7048824a708ab29f3c35342b3eabea8170b2
beckybbk <120100729+beckybbk@users.noreply.github.com>
2023-04-19 19:47:20 +0900

    디스트로이0419

--- Game0411/Assets/Instantiate_Destroy/Script/Create.cs
+++ Game0411/Assets/Instantiate_Destroy/Script/Create.cs
@@ -12,15 +12,35 @@ public class Create : MonoBehaviour
     private float fixedTime = 5f;
     private float currentTime = 5f;
 
-
-    void Start()
+public void CreateGeneric()
     {
-        
+        active= false;
+        Instantiate         // 게임 오브젝트를 생성하는 함수. 
+            (prefab,                    // 생성할 게임 오브젝트
+            new Vector3(0,-1.25f,0),    // 생성되는 위치 값
+            prefab.transform.rotation   // 생성되는 회전 값
+            ).AddComponent<Delete>();
+            
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    
+    private void Update()
     {
-        
+        //Debug.Log(Time.deltaTime);
+
+
+        if(active ==false) 
+        { 
+            button.interactable = false;        //버튼 오브젝트 비활성화
+            currentTime -= Time.deltaTime;      //currentTime에서 한 프레임당 실행되는 시간을 뺌.
+            button.image.fillAmount = currentTime / fixedTime;
+
+            if(currentTime <=0)
+            {
+                active= true;
+                button.interactable = true;
+                button.image.fillAmount = currentTime = fixedTime;
+            }
+        }
     }
 }
--- Game0411/Assets/Instantiate_Destroy/Script/Delete.cs
+++ Game0411/Assets/Instantiate_Destroy/Script/Delete.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Delete : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int random = Random.Range(1, 5);    // 1~5사이의 값이 반환됨.
+
     void Start()
     {
-        
+        Destroy(gameObject, random);
     }
 
     // Update is called once per frame
