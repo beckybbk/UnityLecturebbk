3bd256e0b845a9830f6093f6a07ce2665f5cc4ac
beckybbk <120100729+beckybbk@users.noreply.github.com>
2023-05-03 21:58:06 +0900

    물리0503

---
+++ Game0411/Assets/Physics/Script/PhysicsManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsManager : MonoBehaviour
+{
+    private Ray ray;
+    private RaycastHit rayCastHit;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        if(Input.GetButtonDown("Fire1"))
+        {
+            if(Physics.Raycast(ray, out rayCastHit, Mathf.Infinity)) 
+            { 
+                if(rayCastHit.collider.CompareTag("BilliardBall")) 
+                { 
+                    rayCastHit.collider.GetComponent<Rigidbody>().Sleep();
+                }
+            }
+        }
+    }
+}
